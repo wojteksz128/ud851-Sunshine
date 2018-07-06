c988cd87bd2dd2a3f8114558c54ea14eb46de981
Wojciech Szczepaniak <szczepaniak.brachowice@gmail.com>
2018-07-07 01:35:59 +0200

    Task 6.1 Settings window added (back to ealier invoked activity not working)

--- app/src/main/java/com/example/android/sunshine/DetailActivity.java
+++ app/src/main/java/com/example/android/sunshine/DetailActivity.java
@@ -55,5 +55,17 @@ public class DetailActivity extends AppCompatActivity {
         return true;
     }
 
-    // TODO (7) Launch SettingsActivity when the Settings option is clicked
+    // DONE (7) Launch SettingsActivity when the Settings option is clicked
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings: {
+                startActivity(new Intent(this, SettingsActivity.class));
+                return true;
+            }
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
