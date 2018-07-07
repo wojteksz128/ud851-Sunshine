33d70b789f2b0b7dc711fd2aed465ab7c80b90d2
Wojciech Szczepaniak <szczepaniak.brachowice@gmail.com>
2018-07-07 16:11:43 +0200

    Task 7.1 Create table Weather

--- app/src/main/java/com/example/android/sunshine/data/WeatherDbHelper.java
+++ app/src/main/java/com/example/android/sunshine/data/WeatherDbHelper.java
@@ -15,19 +15,51 @@
  */
 package com.example.android.sunshine.data;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import static com.example.android.sunshine.data.WeatherContract.WeatherEntry.*;
+
 /**
  * Manages a local database for weather data.
  */
-// TODO (11) Extend SQLiteOpenHelper from WeatherDbHelper
-public class WeatherDbHelper {
+// DONE (11) Extend SQLiteOpenHelper from WeatherDbHelper
+public class WeatherDbHelper extends SQLiteOpenHelper {
 
-//  TODO (12) Create a public static final String called DATABASE_NAME with value "weather.db"
+//  DONE (12) Create a public static final String called DATABASE_NAME with value "weather.db"
+    public static final String DATABASE_NAME = "weather.db";
 
-//  TODO (13) Create a private static final int called DATABASE_VERSION and set it to 1
+//  DONE (13) Create a private static final int called DATABASE_VERSION and set it to 1
+    public static final int DATABASE_VERSION = 1;
 
-//  TODO (14) Create a constructor that accepts a context and call through to the superclass constructor
+//  DONE (14) Create a constructor that accepts a context and call through to the superclass constructor
+    public WeatherDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
 
-//  TODO (15) Override onCreate and create the weather table from within it
+//  DONE (15) Override onCreate and create the weather table from within it
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String createTableQuery = "CREATE TABLE " + TABLE_NAME + " (" +
+                _ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                COLUMN_DATE + " INTEGER, " +
+                COLUMN_WEATHER_ID + " INTEGER, " +
+                COLUMN_MIN_TEMP + " REAL, " +
+                COLUMN_MAX_TEMP + " REAL, " +
+                COLUMN_HUMIDITY + " REAL, " +
+                COLUMN_PRESSURE + " REAL, " +
+                COLUMN_WIND_SPEED + " REAL, " +
+                COLUMN_DEGREES + " REAL" +
+                "); ";
 
-//  TODO (16) Override onUpgrade, but don't do anything within it yet
+        db.execSQL(createTableQuery);
+    }
+
+
+//  DONE (16) Override onUpgrade, but don't do anything within it yet
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        
+    }
 }
